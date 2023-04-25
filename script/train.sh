58f9560f226d987f8a5f96701c0bdbdb24800f6f
Yangjun-wow <1971099292@qq.com>
2023-04-25 18:27:06 +0800

    改路径

--- script/train.sh
+++ script/train.sh
@@ -27,7 +27,7 @@ python train.py \
     --data_path datasets/${DATA_SET}/ \
     --dataset_name ${DATA_SET} \
     --model_class ${MODEL_CLASS} \
-    --pretrain_model_path pretrain_model/${PRETRAIN_MODEL} \
+    --pretrain_model_path ${PRETRAIN_MODEL} \
     --pretrain_embed_path /content/tencent-ailab-embedding-zh-d200-v0.2.0-s/tencent-ailab-embedding-zh-d200-v0.2.0-s.txt \
     --seed 42 \
     --markup bios \
